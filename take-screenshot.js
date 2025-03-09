4a28d8363941ebe4339fdb6223f6b80965f84391
Ahmed-walid <ahmed.ibrahim003@eng-st.cu.edu.eg>
2025-03-09 16:18:28 +0200

    First commit

---
+++ take-screenshot.js
@@ -0,0 +1,23 @@
+const puppeteer = require('puppeteer');
+
+async function takeScreenshot(url, filename) {
+  // Launch the browser
+  const browser = await puppeteer.launch();
+  
+  // Create a new page
+  const page = await browser.newPage();
+  
+  // Navigate to the URL
+  await page.goto(url);
+  
+  // Take a screenshot
+  await page.screenshot({ path: filename });
+  
+  // Close the browser
+  await browser.close();
+}
+
+// Usage example: Take a screenshot of a webpage
+takeScreenshot('http://localhost:3000/', 'example-screenshot.png')
+  .then(() => console.log('Screenshot taken!'))
+  .catch((err) => console.error('Error:', err));
